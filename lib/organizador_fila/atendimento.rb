5a5883d8bdb3f004ced120fcc76ed910c90ecfc0
Carlos Guilherme <guilhermeagripino@gmail.com>
2024-05-22 20:58:49 -0300

    Feat: implementado o metodo chamar senha

--- lib/organizador_fila/atendimento.rb
+++ lib/organizador_fila/atendimento.rb
@@ -4,21 +4,24 @@ class Atendimento
     attr_reader :hora_inicial, :tempo_de_espera, :atendidos, :horario_atendimento, :tempo_medio_atedimento
 
     def initialize
+        @atendidos = 0
         @hora_inicial = Time.now
+        @tempo_medio_atedimento = []
     end
 
     def chamar_senha (senhas)
-        @atendidos = 0
-
         if senhas.empty?
-            puts "Nenhuma senha no sistema para ser atendida"
-        elsif (atendidos == 0)
+            puts "Nenhuma senha no sistema."
+        else
             senha_atendida = senhas.shift
-            puts "SENHA: #{senha_atendida}" 
+            puts "SENHA: #{senha_atendida[:senha]}"
+
             @horario_atendimento = Time.now
-            @tempo_de_espera = @hora_inicial - senha_atendida.hora_senha
+            @tempo_de_espera = (horario_atendimento - senha_atendida[:hora]) / 60.0
+            @tempo_medio_atedimento << @tempo_de_espera.round(2)
+            puts "O tempo de espera foi de #{@tempo_de_espera.round(2)}"
 
+            @atendidos += 1
         end
-
     end
 end
