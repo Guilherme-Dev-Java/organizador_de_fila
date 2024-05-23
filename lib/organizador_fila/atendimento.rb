6152a6280bd0c4e651a0d77ef4d8ed4a47171f58
Carlos Guilherme <guilhermeagripino@gmail.com>
2024-05-22 21:09:46 -0300

    Fix: melhorando os metodos da classe atendimento

--- lib/organizador_fila/atendimento.rb
+++ lib/organizador_fila/atendimento.rb
@@ -1,15 +1,15 @@
 require_relative "senha.rb"
 
 class Atendimento
-    attr_reader :hora_inicial, :tempo_de_espera, :atendidos, :horario_atendimento, :tempo_medio_atedimento
+    attr_reader :hora_inicial, :tempo_de_espera, :atendidos, :horario_atendimento, :tempo_medio_atendimento
 
     def initialize
         @atendidos = 0
         @hora_inicial = Time.now
-        @tempo_medio_atedimento = []
+        @tempo_medio_atendimento = []
     end
 
-    def chamar_senha (senhas)
+    def chamar_senha(senhas)
         if senhas.empty?
             puts "Nenhuma senha no sistema."
         else
@@ -17,11 +17,24 @@ class Atendimento
             puts "SENHA: #{senha_atendida[:senha]}"
 
             @horario_atendimento = Time.now
-            @tempo_de_espera = (horario_atendimento - senha_atendida[:hora]) / 60.0
-            @tempo_medio_atedimento << @tempo_de_espera.round(2)
-            puts "O tempo de espera foi de #{@tempo_de_espera.round(2)}"
+            @tempo_de_espera = (@horario_atendimento - senha_atendida[:hora]) / 60.0
+            @tempo_medio_atendimento << @tempo_de_espera.round(2)
+            puts "O tempo de espera foi de #{@tempo_de_espera.round(2)} minutos"
+        end
+    end
 
-            @atendidos += 1
+    def total_de_atendimentos
+        @atendidos = @tempo_medio_atendimento.size
+        puts "O total de atendimentos foi #{@atendidos}"
+    end
+
+    def tempo_medio_de_espera
+        if @tempo_medio_atendimento.empty?
+            puts "O tempo médio para o atendimento é de 25 minutos"
+        else
+            total_espera = @tempo_medio_atendimento.reduce(0, :+)
+            media = total_espera / @tempo_medio_atendimento.size
+            puts "O tempo médio de espera é de #{media.round(2)} minutos"
         end
     end
 end
